f36c38059b076ec8a94b712cbcc79f26df802bf1
Subhan Khan <subhansoft9919@gmail.com>
2025-07-29 19:23:23 +0530

    support portal

--- src/main/java/com/supportportal/domain/HttpResponse.java
+++ src/main/java/com/supportportal/domain/HttpResponse.java
@@ -1,51 +1,32 @@
 package com.supportportal.domain;
 
+import java.util.Date;
+
 import org.springframework.http.HttpStatus;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class HttpResponse {
-    private int httpStatusCode;  //200, 201, 400, 500
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy hh:mm:ss")
+    private Date timeStamp;
+    private int httpStatusCode; // 200, 201, 400, 500
     private HttpStatus httpStatus;
     private String reason;
     private String message;
 
-    public HttpResponse() {}
-
-    public HttpResponse(int httpStatusCode, HttpStatus httpStatus, String reason, String message) {
+    public HttpResponse(Date date, int httpStatusCode, HttpStatus httpStatus, String reason, String message) {
+        this.timeStamp = new Date();
         this.httpStatusCode = httpStatusCode;
         this.httpStatus = httpStatus;
         this.reason = reason;
-        this.message = message;
-    }
-
-    public int getHttpStatusCode() {
-        return httpStatusCode;
-    }
-
-    public void setHttpStatusCode(int httpStatusCode) {
-        this.httpStatusCode = httpStatusCode;
-    }
-
-    public HttpStatus getHttpStatus() {
-        return httpStatus;
-    }
-
-    public void setHttpStatus(HttpStatus httpStatus) {
-        this.httpStatus = httpStatus;
-    }
-
-    public String getReason() {
-        return reason;
-    }
-
-    public void setReason(String reason) {
-        this.reason = reason;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
         this.message = message;
     }
 }
---
+++ src/main/java/com/supportportal/filter/JwtAuthorizationFilter.java
@@ -0,0 +1,50 @@
+package com.supportportal.filter;
+
+
+import java.io.IOException;
+import java.util.List;
+import org.springframework.security.core.Authentication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import com.supportportal.utility.JWTTokenProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import static com.supportportal.constant.SecurityConstant.*;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@Component
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+    private JWTTokenProvider jwtTokenProvider;
+
+    public JwtAuthorizationFilter(JWTTokenProvider JwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (request.getMethod().equalsIgnoreCase(OPTIONS_HTTP_METHOD)) {
+            response.setStatus(HttpStatus.OK.value());
+        } else {
+            String authorizationHeader = request.getHeader(AUTHORIZATION);
+            if(authorizationHeader == null || authorizationHeader.startsWith(TOKEN_PREFIX)) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+            String token = authorizationHeader.substring(TOKEN_PREFIX.length());
+            String username = jwtTokenProvider.getSubject(token);
+            if(jwtTokenProvider.isTokenValid(username, token) && SecurityContextHolder.getContext().getAuthentication() == null) {
+                List<GrantedAuthority> authorities = jwtTokenProvider.getAuthorities(token);
+                Authentication authentication = jwtTokenProvider.getAuthentication(username, authorities, request);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } else {
+                SecurityContextHolder.clearContext();
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+}
